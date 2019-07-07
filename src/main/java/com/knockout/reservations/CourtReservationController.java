2a27738ff283157841116af59e03148f1ca41fe6
Karol Olek <karol.olek@sabre.com>
2019-07-07 22:19:18 +0200

    handling new reservation

--- src/main/java/com/knockout/reservations/CourtReservationController.java
+++ src/main/java/com/knockout/reservations/CourtReservationController.java
@@ -3,6 +3,8 @@ package com.knockout.reservations;
 import com.knockout.reservations.model.Reservation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,13 +23,20 @@ public class CourtReservationController {
         reservationRepository.save(new Reservation(2, 2, new Date()));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/get/{id}")
     public String geReservation(@PathVariable String id) {
         return "Reservation for court " + id + ": " + reservationRepository.findByCourtId(Integer.valueOf(id));
     }
 
-    @GetMapping("/")
+    @GetMapping("/getAll")
     public String getAllReservations() {
         return "All reservations " + reservationRepository.findAll().toString();
     }
+
+    @PostMapping("/reserve")
+    public String reserveCourt(@RequestBody Reservation reservation) {
+        reservationRepository.save(reservation);
+        return "OK" + reservation;
+    }
+
 }
